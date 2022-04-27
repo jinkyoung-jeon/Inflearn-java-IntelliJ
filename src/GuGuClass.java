0f56644770d26a3ca222c926881a82dd2791d5e3
JeonJinKyoung <kyeong1750@naver.com>
2022-04-27 19:49:48 +0900

    부분커밋

---
+++ src/GuGuClass.java
@@ -0,0 +1,11 @@
+package src;
+
+public class GuGuClass {
+    public static void main(String[] args) {
+        for (int i = 2; i < 10; i = i + 1) {
+            for () {
+                
+            }
+        }
+    }
+}
