274a565d5411fbf7d6d572b604391b5901b4d70b
JeonJinKyoung <kyeong1750@naver.com>
2022-04-27 14:47:58 +0900

    연산자를 이용해 값 조작하기

---
+++ src/Operator.java
@@ -0,0 +1,30 @@
+package src;
+
+public class Operator {
+    public static void main(String[] args) {
+        System.out.println(2 + 3);
+        System.out.println(5 - 2);
+        System.out.println(2 * 3);
+        System.out.println(3 / 3);
+        System.out.println(7 % 2);
+
+        System.out.println(7 / 2);
+
+        System.out.println((2 - 4) / 2);
+
+        int number = 5;
+        number = (int) 1.2;
+        System.out.println(number);
+
+        System.out.println(1 + 1.2);
+        System.out.println(7 / 2);
+        System.out.println(7 / 2.0);
+
+        System.out.println(2 < 3);
+        System.out.println(2 <= 3);
+        System.out.println(2 > 3);
+        System.out.println(2 >= 3);
+        System.out.println(2 == 3);
+        System.out.println(2 != 3);
+    }
+}
